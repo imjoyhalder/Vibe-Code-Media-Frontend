ed5095fae2142934a5ac2416a4a5d0c5e0897d22
imjoyhalder <joyhalder00113355@gmail.com>
2026-04-04 23:07:38 +0600

    project initialized

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+    </div>
+  );
+}
